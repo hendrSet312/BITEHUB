5ada5f8544cf941f7ecc3d582b8309c9482bd3d6
Richie Darylson Tandiono <richie.tandiono@binus.edu>
2024-11-30 15:18:49 +0700

    Add kaggle login

--- inference.py
+++ inference.py
@@ -6,16 +6,20 @@ from langchain_core.documents import Document
 from langchain_huggingface import HuggingFaceEmbeddings
 import keras
 import keras_hub
+import kagglehub
 
-PERSIST_PATH = './Food Nutrition/chroma'
+kagglehub.login()
+
+PERSIST_PATH = './chroma'
 
 class RAGQuery:
     
     def __init__(self, persist_path):
         self.PERSIST_PATH = persist_path
         self.embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
-
+        print("Initializing vector store")
         self.food_vector_store = Chroma(collection_name="food_calories", persist_directory=self.PERSIST_PATH, embedding_function=self.embeddings)
+        print("importing model")
         self.gemma_lm = keras_hub.models.GemmaCausalLM.from_preset("gemma2_instruct_2b_en")
 
     def query(self, user_query):
